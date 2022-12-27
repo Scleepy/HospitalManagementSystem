6b7891653c1af7411ecf4244302e92de3778e018
vincentandrosa <121498776+vincentandrosa@users.noreply.github.com>
2022-12-28 01:50:49 +0700

    Update Medicine.java

--- src/Medicine.java
+++ src/Medicine.java
@@ -1,3 +1,37 @@
 public class Medicine {
-    
+    public String medicineID, medicineName, medicineDescription, medicineInstruction;
+	public int medicineQuantity, medicinePrice;
+	
+	Medicine(String medicineID, String medicineName, int medicineQuantity, String medicineDescription, String medicineInstruction, int medicinePrice) {
+		this.medicineID = medicineID;
+		this.medicineName = medicineName;
+		this.medicineQuantity = medicineQuantity;
+		this.medicineDescription = medicineDescription;
+		this.medicineInstruction = medicineInstruction;
+		this.medicinePrice = medicinePrice;
+	}
+	
+	public String getmedicineID() {
+		return medicineID;
+	}
+	
+	public String getmedicineName() {
+		return medicineName;
+	}
+	
+	public int getmedicineQuantity() {
+		return medicineQuantity;
+	}
+	
+	public String getmedicineDescription() {
+		return medicineDescription;
+	}
+	
+	public String getmedicineInstruction() {
+		return medicineInstruction;
+	}
+	
+	public int getmedicinePrice() {
+		return medicinePrice;
+	}
 }
