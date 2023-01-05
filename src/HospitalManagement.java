f9d8dc24012779d031fd793b8c0fbbccf79e15ce
Scleepy <danielyohanes03@gmail.com>
2023-01-05 17:14:50 +0700

    Added getPassword to Person class

--- src/HospitalManagement.java
+++ src/HospitalManagement.java
@@ -8,34 +8,6 @@ import java.util.ArrayList;
 
 public class HospitalManagement {
 
-    //FIX THIS
-
-    //ArrayList<patient> list = new ArrayList<patient>();
-	
-	// public void showPatient() {
-	// 	for(int i=0; i<list.size(); i++) {
-	// 	    System.out.println((i + 1) + ". " + list.get(i).patientName);
-	// 		System.out.print(list.get(i).patientID);
-	// 		System.out.print(list.get(i).doctorID);
-	// 		System.out.print(list.get(i).medicineList);
-    //    }
-	// }
-	
-	// public void selectPatient() {
-	// 	Scanner scanner = new Scanner(System.in);
-	// 	System.out.print("Enter the number of the patient to select: ");
-		
-    //     int selection = scanner.nextInt();
-    //     if (selection > 0 && selection <= list.size()) {    	
-    //     	System.out.print(list.get(selection - 1));
-    //     } 
-        
-    //     else {
-    //         System.out.println("Invalid selection");
-    //     }
-        
-    //     isConsulted = True;
-
     public HospitalManagement(){};
 
     public static void main(String[] args) throws Exception {
